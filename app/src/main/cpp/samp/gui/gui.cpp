a380ce968a130ecde2b6616b0af16368174d81ef
v7-desenvolvedor <antoniolfreitas1965@gmail.com>
2026-05-08 20:40:50 -0300

    Update gui.cpp

--- app/src/main/cpp/samp/gui/gui.cpp
+++ app/src/main/cpp/samp/gui/gui.cpp
@@ -103,7 +103,7 @@ bool UI::initialize()
 	// mem
 	Label* d_label1;
 
-	d_label1 = new Label(cryptor::create("0.8.2.1 x1y2z").decrypt(), ImColor(1.0f, 1.0f, 1.0f), true, UISettings::fontSize() / 3);
+	d_label1 = new Label(cryptor::create("1.2 Aura ROLEPLAY").decrypt(), ImColor(1.0f, 1.0f, 1.0f), true, UISettings::fontSize() / 7);
 	this->addChild(d_label1);
 	d_label1->setPosition(ImVec2(3.0, 3.0));
 
@@ -250,7 +250,8 @@ void UI::renderDebug()
         snprintf(&szStr[0], sizeof(szStr), "FPS: %.0f", fps);
         if(pJavaWrapper) {
             // Adicionei a vírgula entre o ID (2) e o Tempo (12:20)
-            pJavaWrapper->UpdateHudData("2", "12:20", "55", (int)fps);
+            // FORMA CORRETA:
+            pJavaWrapper->UpdateHudData(2, "12:20", 55, (int)fps);
         }
 
         label->setText(&szStr[0]);
