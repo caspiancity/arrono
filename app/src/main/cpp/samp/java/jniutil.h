c333c30a46b9f75da58ebe0f383b4a83f98d1eef
v7-desenvolvedor <antoniolfreitas1965@gmail.com>
2026-05-10 15:58:01 -0400

    Update jniutil.h

--- app/src/main/cpp/samp/java/jniutil.h
+++ app/src/main/cpp/samp/java/jniutil.h
@@ -18,7 +18,7 @@ public:
     
     const char* GetClipboardString();
     void UpdateHudData(int ping, const char* time, int cpu, int fps);
-    void UpdateHudInfo(int health, int armour, int hunger, int weaponidweik, int ammo, int ammoinclip, int money, int wanted);
+    void updateHudInfo(int health, int armour, int hunger, int weaponidweik, int ammo, int ammoinclip, int money, int wanted);
     void SetPauseState(bool pause);
     void ShowLoadingScreen();
     void HideLoadingScreen();
